7ecb0d52671c679bbb971487953a0312a08d0998
Julián López <julian.lopez@estudiants.urv.cat>
2016-02-13 19:49:35 +0100

    VERSION NUEVA

--- Codeathonurv2016_MANZANA/app/src/main/java/com/example/julin/codeathonurv2016_manzana/MenuAsignaturasActivity.java
+++ Codeathonurv2016_MANZANA/app/src/main/java/com/example/julin/codeathonurv2016_manzana/MenuAsignaturasActivity.java
@@ -1,7 +1,10 @@
 package com.example.julin.codeathonurv2016_manzana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuAsignaturasActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MenuAsignaturasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_asignaturas);
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nuevoform;
+                
+                nuevoform = new Intent(MenuAsignaturasActivity.this, ETSEActivity.class);
+                startActivity(nuevoform);
+            }
+        });
     }
 }
