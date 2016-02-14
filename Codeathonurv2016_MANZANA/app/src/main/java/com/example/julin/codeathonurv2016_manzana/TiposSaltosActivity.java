466e5ce31a12f950820b96314bc4b6f6272de6ad
Julián López <julian.lopez@estudiants.urv.cat>
2016-02-14 16:29:06 +0100

    VERSION DEF

--- Codeathonurv2016_MANZANA/app/src/main/java/com/example/julin/codeathonurv2016_manzana/TiposSaltosActivity.java
+++ Codeathonurv2016_MANZANA/app/src/main/java/com/example/julin/codeathonurv2016_manzana/TiposSaltosActivity.java
@@ -6,20 +6,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.LinkedList;
+
 public class TiposSaltosActivity extends AppCompatActivity {
 ListView listanueva;
-    String[] listaS = {"JOSE: ¿Podeis explicarme la diferencia entre MOVEQ y un MOV con un BEQ despues?","ROBERTO: Dame cinco minutos y te explico","JOSE: Vale, gracias."};
+    public LinkedList<String> listaS = new LinkedList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        listaS.add("JOSE: ¿Podeis explicarme la diferencia entre MOVEQ y un MOV con un BEQ despues?");
+        listaS.add("ROBERTO: Dame cinco minutos y te explico");
+        listaS.add("JOSE: Vale, gracias.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipos_saltos);
         setTitle("Tipos de Salto");
         listanueva = (ListView)findViewById(R.id.listVie99);
         ArrayAdapter<String> l= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listaS);
         listanueva.setAdapter(l);
-        
+        Button mon = (Button) findViewById(R.id.button5);
+        mon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listaS.add(((AutoCompleteTextView) findViewById(R.id.autoCompleteTextView4)).getText().toString());
+                listanueva = (ListView)findViewById(R.id.listVie99);
+
+                setContentView(R.layout.activity_tipos_saltos);
+            }
+        });
+
     }
 }
